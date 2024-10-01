236d9ac92a644955e661a2417ed3d4821c60e8de
E202222G <mallory.gesbert@etu.univ-nantes.fr>
2024-10-01 16:00:24 +0300

    Add deleteAccount and updateAccount function

--- controllers/authController.js
+++ controllers/authController.js
@@ -60,3 +60,81 @@ exports.role = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 };
+
+exports.deleteAccount = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Authorization token malformed' });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findByIdAndDelete(decoded.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'Account successfully deleted' });
+    } catch (error) {
+        console.log(error);
+
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: 'Token expired' });
+        } else if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ message: 'Token is not valid' });
+        }
+
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.updateAccount = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decoded.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const data = req.body;
+        if (data.firstName && data.firstName.length > 1) {
+            user.firstName = data.firstName;
+        }
+        if (data.lastName && data.lastName.length > 1) {
+            user.lastName = data.lastName;
+        }
+        if (data.currentPassword && data.newPassword) {
+            const isMatch = await bcrypt.compare(data.currentPassword, user.password);
+            if (!isMatch) return res.status(400).json({ message: 'Invalid credentials: bad current password' });
+            
+            const isMatchNew = await bcrypt.compare(data.newPassword, user.password);
+            if (isMatchNew) return res.status(400).json({ message: 'Invalid credentials: new password is the same as the older password' });
+            
+            if (isValidPassword(data.newPassword)){
+                const hashedPassword = await bcrypt.hash(data.newPassword, 10);
+                user.password = hashedPassword;
+            }else{             
+                return res.status(400).json({ message: 'Invalid credentials: new password is invalid' });
+            }
+
+        }
+
+        await user.save();
+
+        console.log("Account updated!");
+        res.status(200).json({ message: 'Account updated', user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+const isValidPassword = (password) => {
+    if (password.length < 10) {
+        return false;
+    }
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasSpecialCharacter = /[!@#$%^&]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    return hasUpperCase && hasSpecialCharacter && hasNumber;
+};
